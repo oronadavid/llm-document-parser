ced2829803eab997b940f9bc100b7f067a726539
oronadavid <davidorona2@gmail.com>
2025-04-21 20:51:47 -0700

    Add fields to config

---
+++ src/llm_document_parser/config.py
@@ -0,0 +1,24 @@
+# config.py
+
+# Options: "rapid", "easy", "ocrmac", "tesseract"
+OCR_MODEL = "rapid"
+
+# Must be set when using the tesseract OCR model
+# Linux: "/usr/share/tesseract-ocr/4.00/tessdata"
+# Windows: "C:\\Program Files\\Tesseract-OCR\\tessdata"
+# Mac: "/usr/local/share/tessdata" or "/opt/homebrew/share/tessdata"
+TESSERACT_TESSDATA_LOCATION = "/usr/share/tesseract-ocr/4.00/tessdata"
+
+OLLAMA_MODEL = "llama3.2"
+
+LLM_PROMPT = """
+        Extract all transactions from the following statement. Each transaction must be returned as a JSON object with the fields: transaction_date (YYYY-MM-DD), description, amount, and transaction_type ('deposit' or 'withdrawal'). All of these must be returned as a list of JSON objects under a key called 'transactions'.
+"""
+
+# Options: "csv", "json", "excel"
+EXPORT_TYPE = "csv"
+
+# Can be a file or directory
+INPUT_PATH = "/home/david/Projects/school/capstone/llm-document-parser/src/llm_document_parser/test_data"
+OUTPUT_FOLDER = "/home/david/Desktop"
+OUTPUT_FILE_NAME = "output"
