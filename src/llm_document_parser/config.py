6c07818ea89b60334c48c51efcca26e95533aa1f
oronadavid <davidorona2@gmail.com>
2025-04-28 20:18:05 -0700

    Move instructor data model declarations to config

--- src/llm_document_parser/config.py
+++ src/llm_document_parser/config.py
@@ -1,7 +1,10 @@
 # config.py
+from pydantic import BaseModel
+from datetime import date
+from typing import List
 
 # Options: "rapid", "easy", "ocrmac", "tesseract"
-OCR_MODEL = "tesseract"
+OCR_MODEL = "rapid"
 
 # Must be set when using the tesseract OCR model
 # Linux: "/usr/share/tesseract-ocr/4.00/tessdata"
@@ -12,13 +15,36 @@ TESSERACT_TESSDATA_LOCATION = "/usr/share/tesseract-ocr/4.00/tessdata"
 OLLAMA_MODEL = "dolphin3"
 
 LLM_PROMPT = """
-        Extract all transactions from the following statement. Each transaction must be returned as a JSON object with the fields: transaction_date (YYYY-MM-DD), description, amount, and transaction_type ('deposit' or 'withdrawal'). All of these must be returned as a list of JSON objects under a key called 'transactions'.
+        Extract all transactions from the following statement. Each transaction must be returned as a JSON object with the fields: transaction_date (YYYY-MM-DD), description, amount, and transaction_type ('deposit' or 'withdrawal'). All of these must be returned as a list of JSON objects under a key called 'transactions'. Here is an example:
+        [
+            {
+                transaction_date: 2025-01-24,
+                description: "Walmart",
+                amount: 34.24,
+                transaction_type: "withdrawl"
+            }
+        ]
 """
 
 # Options: "csv", "json", "excel"
-EXPORT_TYPE = "csv"
+EXPORT_TYPE = "json"
 
 # Can be a file or directory
-INPUT_PATH = ""
-OUTPUT_FOLDER = ""
+INPUT_PATH = "/home/david/Projects/school/capstone/llm-document-parser/src/llm_document_parser/test_data/test2.png"
+OUTPUT_FOLDER = "/home/david/Desktop/"
 OUTPUT_FILE_NAME = "output"
+
+# Define Pydantic response models for instructor:
+
+class BankStatementEntry(BaseModel):
+    transaction_date: date | None | str
+    description: str | None
+    amount: float | None
+    #transaction_type: Literal['deposit', 'withdrawal', None]
+    transaction_type: str | None
+
+class BankStatement(BaseModel):
+    transactions: List[BankStatementEntry] | None
+
+# The model that LLM output will conform to
+RESPONSE_MODEL = BankStatement
